a7b747e2619fce814fe3603060f77e1dfde04127
Joao Rietra <jhlrietra@gmail.com>
2025-09-26 10:07:25 -0300

    Create example_req.sh

---
+++ example_req.sh
@@ -0,0 +1,5 @@
+addr=''
+curl -X POST "http://localhost:8000/validate" \
+     -H "Authorization: Bearer <g_id_token>" \
+     -H "Content-Type: application/json" \
+     -d "{\"image_url\": \"$addr\"}"
